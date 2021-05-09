3ccfc8f8eedabc37a256207281b42f1c858af9ee
JackRepo170 <cr85170@gmail.com>
2021-05-09 15:59:30 +1000

    apiandvagrant

---
+++ Server/WebApiPrototype/WebApiPrototype/Data/SensorContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiPrototype.Models;
+
+namespace WebApiPrototype.Data
+{
+      public class SensorContext : DbContext
+      {
+            public SensorContext(DbContextOptions<SensorContext> options) : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; }
+            public DbSet<TemperatureSensor> TemperatureSensor { get; set; }
+      }
+   
+}
---
+++ Server/WebApiPrototype/WebApiPrototype/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiPrototype.Models;
+using WebApiPrototype.Data;
+
+namespace WebApiPrototype
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<SensorContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiPrototype", Version = "v1" });
+            });
+
+            services.AddDbContext<SensorContext>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("DBConnection")); // found in appsettings.json
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiPrototype v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
