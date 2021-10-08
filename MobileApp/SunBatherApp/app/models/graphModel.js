0f9b55fba5e017f5e68697fb82fac7d927c4a44a
lachjb <lachieb15@gmail.com>
2021-10-08 21:53:48 +1100

    Fixing Graphs
    changed emissions to co2/kw (kg)
    removed cost and emissions from graphs page
    made negative emissions or cost equal to 0
    (y)

--- MobileApp/SunBatherApp/app/models/graphModel.js
+++ MobileApp/SunBatherApp/app/models/graphModel.js
@@ -4,8 +4,8 @@ export function graphOptions() {
       { name: "Temperature Value Out", nameAbbreviated: "Temp Out", databaseField: "temperatureValueOutput", isSelected: false },
       { name: "Temperature Value Roof", nameAbbreviated: "Temp Roof", databaseField: "temperatureValueRoof", isSelected: false },
       { name: "Solar Irradiance", nameAbbreviated: "Solar Energy", databaseField: "solarIrradiance", isSelected: false },
-      { name: "Cost", nameAbbreviated: "Cost", databaseField: "cost", isSelected: false },
-      { name: "Emissions", nameAbbreviated: "Emissions", databaseField: "emissions", isSelected: false },
+     // { name: "Cost", nameAbbreviated: "Cost", databaseField: "cost", isSelected: false },
+     // { name: "Emissions", nameAbbreviated: "Emissions", databaseField: "emissions", isSelected: false },
       { name: "Energy Adsorbed", nameAbbreviated: "Energy", databaseField: "energyAdsorbed", isSelected: false },
     ];
   
